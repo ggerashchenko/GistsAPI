6f7e64ece715c70124f391964971209b557df70c
Grygorii Gerashchenko <grygorii.gerashchenko@veon.com>
2018-08-20 22:21:40 +0200

    Cleaned API client

--- src/main/java/client/GistsApiClient.java
+++ src/main/java/client/GistsApiClient.java
@@ -2,41 +2,21 @@ package client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import models.GetGistsDto;
+import models.GistsDto;
 import models.PostGistDto;
-import models.UpdateGistDto;
-import org.json.simple.parser.ParseException;
 import org.junit.Assume;
 import utils.TestPropertiesLoader;
-import utils.Utils;
 
 import java.io.IOException;
 
 public class GistsApiClient {
-	private String token = TestPropertiesLoader.getToken();
-	private Utils utils = new Utils();
-
 	static {
 		RestAssured.baseURI = TestPropertiesLoader.getBaseUrl();
 	}
 
-//	public io.restassured.path.json.JsonPath getGists(int expectedStatusCode) {
-//		JsonPath response = RestAssured.given()
-//				.header("Authorization", "Bearer " + token)
-//				.expect()
-//				.statusCode(expectedStatusCode)
-//				.when()
-//				.get("/gists")
-//				.then()
-//				.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("getGistSchema.json"))
-//				.extract().jsonPath();
-//
-//		return response;
-//	}
+	private String token = TestPropertiesLoader.getToken();
 
 	public Response getGists() {
 		return RestAssured.given()
@@ -44,31 +24,12 @@ public class GistsApiClient {
 				.get("/gists");
 	}
 
-	public GetGistsDto.GistDetails[] getParsedGists() throws IOException {
+	public GistsDto.GistDetails[] getParsedGists() throws IOException {
 		Response getGistsResponse = getGists();
 		Assume.assumeTrue(getGistsResponse.getStatusCode() == 200);
 		com.fasterxml.jackson.databind.ObjectMapper jasksonObjectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
-		return jasksonObjectMapper.readValue(getGistsResponse.getBody().print(), GetGistsDto.GistDetails[].class);
+		return jasksonObjectMapper.readValue(getGistsResponse.getBody().print(), GistsDto.GistDetails[].class);
 	}
-
-//	public JsonPath postGist(String gist, int expectedStatusCode) throws IOException, ParseException {
-//
-//		String myGist = utils.readFromFile(gist);
-//
-//		JsonPath response = RestAssured.given()
-//				.header("Authorization", "Bearer " + token)
-//				.contentType("application/json")
-//				.body(myGist)
-//				.expect()
-//				.statusCode(expectedStatusCode)
-//				.when()
-//				.post("/gists")
-//				.then()
-//				.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("postGistSchema.json"))
-//				.extract().jsonPath();
-//
-//		return response;
-//	}
 
 	public void postGist(PostGistDto.GistDetails gist) throws JsonProcessingException {
 		ObjectMapper jacksonObjectMapper = new ObjectMapper();
@@ -95,7 +56,6 @@ public class GistsApiClient {
 	}
 
 	public Response getGistById(String gistId){
-
 		Response response = RestAssured.given()
 				.header("Authorization", "Bearer " + token)
 				.contentType("application/json")
